3552a95250763c685f69f1c01cb4851d4fe5c0cf
Zahra Ghaed <zghaed@gmail.com>
2017-04-06 17:54:21 -0700

    Working on the frontend with hard coded data

--- client/script.js
+++ client/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+  var headerHeight = $('.header').outerHeight();
+  var footerHeight = $('.footer').outerHeight();
+  var totalHeight = $('body').outerHeight();
+  var mainHeight = (totalHeight - (headerHeight + footerHeight +32 )) / totalHeight * 100;
+  console.log(mainHeight+'%');
+  $('.main').outerHeight(mainHeight+'%');
 
-})
+  var itemHeight = $('#test').height();
+  var itemWidth = $('#test').width();
+  $('.addElement').height(itemHeight);
+  $('.addElement').width(itemWidth);
+});
